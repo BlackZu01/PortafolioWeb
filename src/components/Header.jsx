cc29ee7faa75655d902512d73c172b26d9279251
BlackZu01 <citozuluaga44@gmail.com>
2024-12-07 11:27:21 -0500

    Some changes in icons used

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,16 +2,18 @@
  * Created by Jesus Zuluaga (Mathematician and Developer)
  * @license Apache-2.0
  */
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Header = () => {
     return (
         <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
             
-            <div className="max-w-screen-2xl w-full mx-auto px-4">
+            {/* To make the code responsive  */}
+            <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid-cols-[1fr, 3fr, 1fr]">
                 <h1>
-                    <a href="" className="logo">
+                    <a href="/" className="logo">
                         <img 
-                        src="/images/logo.svg" 
+                        src="public/images/logo.svg"
                         width={40}
                         height={40}
                         alt="Jesus Zuluaga" 
@@ -19,18 +21,20 @@ const Header = () => {
                     </a>
                 </h1>
 
-                <div className="">
+                <div className="relative md:justify-self-center">
                     <button 
-                    className=""
-                    onClick={null}
-                    >
-                        <span className="material-symbols-rounded">
-                            menu
-                        </span>
-                    </button>
+                    className="menu-btn"
+                    onClick={null}>
+                       <MenuIcon  />
+                    </button> Navbar
                 </div>
+                <a 
+                href="#contact"
+                className=""
+                >
+                    Contact me
+                </a>
             </div>
-
         </header>
     )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+/**
+ * Created by Jesus Zuluaga (Mathematician and Developer)
+ * @license Apache-2.0
+ */
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar;
